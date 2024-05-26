554f7348af24dd2ed5ccb97fb2efbc7de22ae69b
Joao Taboada <taboadajoao19122235@gmail.com>
2024-05-26 19:10:37 +0100

    feat: added base signup modal/dialog

--- src/js/signup.js
+++ src/js/signup.js
@@ -1,7 +1,14 @@
 const signupButton = document.getElementById('signup-button');
-const signupModal = document.getElementById('first-signup-dialog');
+const signupModal = document.getElementById('signup-dialog');
+const closeButton = document.getElementById('left-dialog-button');
+const signupForm = document.getElementById('signup-form');
 
 signupButton.addEventListener('click', (e) => {
     e.preventDefault();
     signupModal.showModal();
+})
+
+closeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    signupModal.close();
 })
