c88c1e6afaf66253cc25f92ee76d79a08825fb1b
Joao Taboada <taboadajoao19122235@gmail.com>
2024-07-05 00:54:37 +0100

    fix: centered the account options tooltip

--- src/js/header/accountTooltip.mjs
+++ src/js/header/accountTooltip.mjs
@@ -1,13 +1,16 @@
 const headerAccountSpan = document.getElementById('header-account-span');
 const main = document.querySelector('main');
+const spanAccountName = document.querySelector('.log-in-sign-up > span > p');
 
 const logoutButton = document.getElementById('account-logout');
 
 // Event Listeners
 headerAccountSpan.addEventListener('mouseenter', (e) => {
     e.preventDefault();
+    const spanAccount = document.querySelector('.log-in-sign-up > span');
 
     const accountTooltip = document.getElementById('account-tooltip');
+    accountTooltip.style.right = spanAccount.offsetWidth / 2 + 10 + "px";
     accountTooltip.style.display = "flex";
 })
 
