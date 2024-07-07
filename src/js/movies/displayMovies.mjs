91d440bcafb2e8a8c7539a7441c63111f18a4708
João Taboada <mokap@DESKTOP-G0DI43R>
2024-07-07 02:19:18 +0100

    feat: added dynamic movie sorting and movie rating display on main page

--- src/js/movies/displayMovies.mjs
+++ src/js/movies/displayMovies.mjs
@@ -2,16 +2,13 @@ import { cinemaGrader } from "../../instances/cinemaGrader.mjs";
 
 // Movie Functions
 
-function movieDOM(img, title, release, rating) {
-    const movieList = document.getElementById('movie-list');
+function movieDOM(img, title, release, rating, synopsis) {
     const div = document.createElement('div');
     const poster = document.createElement('img');
     const h1 = document.createElement('h1');
     const h2 = document.createElement('h2');
     const div2 = document.createElement('div');
     const ratingImg = document.createElement('img');
-
-
 
     div.classList.add('movie');
 
@@ -21,12 +18,36 @@ function movieDOM(img, title, release, rating) {
     h1.innerText = title;
     h2.innerText = release;
 
-    ratingImg.src = './src/svgs/stars/full.svg';
+    const roundedHalfRating = (Math.round(rating * 2) / 2).toFixed(1);
+
+    console.log(roundedHalfRating)
+
+    ratingImg.src = `./src/svgs/stars/${roundedHalfRating}.svg`;
     ratingImg.alt = 'rating';
 
     div2.append(ratingImg);
     div.append(poster, h1, h2, div2);
-    movieList.append(div);
+
+    const movieGenre = synopsis.split(' ')[0];
+
+    if (!document.getElementById(`${movieGenre}-list`)) {
+        const main = document.querySelector('main');
+        const titleSpan = document.createElement('span');
+        const titleDecoration = document.createElement('div');
+        const titleText = document.createElement('h1');
+        const movieListElem = document.createElement('div');
+
+        titleText.innerText = movieGenre;
+        movieListElem.classList.add('movies');
+        movieListElem.id = `${movieGenre}-list`;
+
+        titleSpan.append(titleDecoration, titleText);
+        main.append(titleSpan, movieListElem);
+    }
+
+    const listElem = document.getElementById(`${movieGenre}-list`)
+
+    listElem.append(div);
 }
 
 async function getMovies() {
@@ -45,9 +66,9 @@ async function start() {
     const movieList = await getMovies();
 
     for (let movie of movieList) {
-        movieDOM(movie.picture, movie.title, movie.releaseDate.split('-')[0], 0)
+        movieDOM(movie.picture, movie.title, movie.releaseDate.split('-')[0], movie.averageGrade, movie.synopsis)
+        console.log(movie)
     }
-    console.log(movieList)
 }
 
 start();
