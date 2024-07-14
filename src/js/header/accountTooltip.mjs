73b705d44fecfa0957f7809720dfa8dfd32a7771
João Taboada <mokap@DESKTOP-G0DI43R>
2024-07-14 16:34:34 +0100

    fix: fixed some bug on the tooltip, changing eventListener from hover to mouse click, and adding functionality to the icon on the header

--- src/js/header/accountTooltip.mjs
+++ src/js/header/accountTooltip.mjs
@@ -4,22 +4,19 @@ const main = document.querySelector('main');
 const logoutButton = document.getElementById('account-logout');
 
 // Event Listeners
-headerAccountSpan.addEventListener('mouseenter', (e) => {
+headerAccountSpan.addEventListener('click', (e) => {
     e.preventDefault();
     const spanAccount = document.querySelector('.log-in-sign-up > span');
 
     const accountTooltip = document.getElementById('account-tooltip');
-    accountTooltip.style.right = spanAccount.offsetWidth / 2 + 10 + "px";
-    accountTooltip.style.display = "flex";
-})
 
-//FIXME: make tooltip work properly 
-
-main.addEventListener('mouseenter', (e) => {
-    e.preventDefault();
-
-    const accountTooltip = document.getElementById('account-tooltip');
-    accountTooltip.style.display = "none";
+    if (accountTooltip.style.display === "flex") {
+        accountTooltip.style.display = "none";
+    } else {
+        accountTooltip.style.right = spanAccount.offsetWidth / 2 + 10 + "px";
+        accountTooltip.style.display = "flex";
+    }
+    
 })
 
 logoutButton.addEventListener('click', (e) => {
---
+++ src/js/header/index.mjs
@@ -0,0 +1,8 @@
+const titleIcon = document.getElementById('titleicon');
+
+// Event Listeners
+titleIcon.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    location.href = '../../../index.html';
+})
